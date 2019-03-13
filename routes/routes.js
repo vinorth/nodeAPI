69a072084719af02c0a003da1817238b2865f31c
Vinorth Vigneswaramoorthy <vinorthv@gmail.com>
2019-03-13 08:50:33 -0400

    Pulled index page and route

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,15 @@
 const ValuesCtrl = require('../controllers/values_controller');
 const UserCtrl = require('../controllers/users_controller');
-var path = require('path');
 
 module.exports = function (app) {
     app
-     .get('/', (req, res) => res.sendFile(path.join(__dirname + '../../index.html')) )
      .get('/test/:id', ValuesCtrl.readValue)
      .get('/tests', ValuesCtrl.readValues)
      .post('/test', ValuesCtrl.createValue)
      .post('/tests', ValuesCtrl.createValues)
      .put('/test', ValuesCtrl.updateValue)
      .delete('/test/:id', ValuesCtrl.deleteValue)
+
      .post('/user', UserCtrl.createUser)
      .put('/user', UserCtrl.updateUser)
      .get('/users', UserCtrl.getAllUsers)
